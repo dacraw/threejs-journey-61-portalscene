b5e21b179d05b068278b94cb68f79b2187698318
Doug <doug.a.crawford@gmail.com>
2024-03-18 15:19:20 -0700

    Add pole lights & portal

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,4 +1,7 @@
-import { OrbitControls, useGLTF, useTexture } from "@react-three/drei";
+import { OrbitControls, useGLTF, useTexture, Center } from "@react-three/drei";
+import * as THREE from "three";
+
+const poleLightMaterial = new THREE.MeshBasicMaterial();
 
 export default function Experience() {
   const { nodes } = useGLTF("./model/portal.glb");
@@ -11,9 +14,29 @@ export default function Experience() {
       <color args={["#030202"]} attach="background" />
       <OrbitControls makeDefault />
 
-      <mesh geometry={nodes.baked.geometry}>
-        <meshBasicMaterial map={bakedTexture} />
-      </mesh>
+      <Center>
+        <mesh geometry={nodes.baked.geometry}>
+          <meshBasicMaterial map={bakedTexture} />
+        </mesh>
+
+        <mesh
+          geometry={nodes.poleLightA.geometry}
+          position={nodes.poleLightA.position}
+          material={poleLightMaterial}
+        />
+
+        <mesh
+          geometry={nodes.poleLightB.geometry}
+          position={nodes.poleLightB.position}
+          material={poleLightMaterial}
+        />
+
+        <mesh
+          geometry={nodes.portalLight.geometry}
+          position={nodes.portalLight.position}
+          rotation={nodes.portalLight.rotation}
+        ></mesh>
+      </Center>
     </>
   );
 }
