a18f7df9726c9aaba1e7e874157c15adcd3c8f49
Doug <doug.a.crawford@gmail.com>
2024-03-18 15:28:49 -0700

    Add portal shader

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -6,6 +6,8 @@ import {
   Sparkles,
 } from "@react-three/drei";
 import * as THREE from "three";
+import portalVertexShader from "./shaders/portal/vertex.glsl";
+import portalFragmentShader from "./shaders/portal/fragment.glsl";
 
 const poleLightMaterial = new THREE.MeshBasicMaterial();
 
@@ -41,7 +43,17 @@ export default function Experience() {
           geometry={nodes.portalLight.geometry}
           position={nodes.portalLight.position}
           rotation={nodes.portalLight.rotation}
-        />
+        >
+          <shaderMaterial
+            vertexShader={portalVertexShader}
+            fragmentShader={portalFragmentShader}
+            uniforms={{
+              uTime: { value: 0 },
+              uColorStart: { value: new THREE.Color("#ffffff") },
+              uColorEnd: { value: new THREE.Color("#000000") },
+            }}
+          />
+        </mesh>
 
         <Sparkles
           size={6}
